347267142173e5deb671d3efc7ff3ff0f079d93e
Azamat S. Kalimoulline <turtle@bazon.ru>
2012-07-04 19:01:20 +0400

    Fully implemented graham-queue.

--- src/impl/graham-queue.lisp
+++ src/impl/graham-queue.lisp
@@ -3,23 +3,28 @@
 (in-package :ru.bazon.bazon-collections)
 
 (def-collection-impl-class graham-queue (abstract-queue)
-  ((list
+  ((list-queue
     :type list
-    :initform '()
+    :initform (cons nil nil)
     :documentation "Back built-in list."))
   (:documentation "Graham queue."))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defmethod size ((queue graham-queue))
-  (with-slots (list)
+  (with-slots (list-queue)
       queue
-    (length list)))
+    (length (car list-queue))))
+
+(defmethod empty-p ((queue graham-queue))
+  (with-slots (list-queue)
+      queue
+    (null (car list-queue))))
 
 (defmethod iterator ((queue graham-queue) &optional condition)
-  (with-slots (list)
+  (with-slots (list-queue)
       queue
-    (let ((iterator (make-instance 'built-in-list-iterator :list list)))
+    (let ((iterator (make-instance 'built-in-list-iterator :list (car list-queue))))
       (if condition
 	  (make-instance 'conditional-iterator
 			 :condition condition
@@ -27,19 +32,40 @@
 	  iterator))))
 
 (defmethod clear ((queue graham-queue))
-  (with-slots (list)
+  (with-slots (list-queue)
       queue
-    (setf list '())))
+    (setf list-queue (cons nil nil))))
 
-(defmethod remove-object ((queue graham-queue) object)
-  )
+(defmethod remove-object ((queue graham-queue) (index abstract-iterator))
+  (let ((iterator (it-native-iterator index)))
+    (with-slots (prev-cons current-cons)
+	iterator
+      (with-slots (list-queue)
+	  queue
+	(let ((next-cons (rest current-cons)))
+	  (setf (cdr prev-cons) next-cons)
+	  (setf current-cons prev-cons)
+	  (when (eq (car prev-cons) :BEFORE-LIST)
+	    (setf (car list-queue) next-cons))
+	  (when (null next-cons)
+	    (setf (cdr list-queue) prev-cons)))))))
 
 (defmethod peek-object ((queue graham-queue))
-  )
+  (with-slots (list-queue)
+      queue
+    (car (car list-queue))))
 
-(defmethod enqueue-object ((queue graham-queue) object))
+(defmethod enqueue-object ((queue graham-queue) object)
+  (with-slots (list-queue)
+      queue
+    (if (null (car list-queue))
+	(setf (cdr list-queue) (setf (car list-queue) (list object)))
+	(setf (cdr (cdr list-queue)) (list object)
+	      (cdr list-queue) (cdr (cdr list-queue))))))
 
 (defmethod dequeue-object ((queue graham-queue))
-  )
+  (with-slots (list-queue)
+      queue
+    (pop (car list-queue))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
