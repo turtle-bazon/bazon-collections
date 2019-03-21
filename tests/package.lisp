24ee5322664ae33f2ca60e9db6738aaad054cfc4
Azamat S. Kalimoulline <turtle@bazon.ru>
2019-03-21 12:30:21 +0300

    Delete legacy code

--- tests/package.lisp
+++ tests/package.lisp
@@ -58,39 +58,3 @@
 
 (def-suite all-tests
   :description "All tests")
-
-;;;
-;;; run-tests
-;;;
-#+nil(defun run-all-tests ()
-  (let* ((test-results (iter (for test-suite in (testsuites))
-			     (for should-run-tests = (not (or (equal test-suite 'TEST-MIXIN)
-							      (equal test-suite 'LIFT::PROCESS-TEST-MIXIN))))
-			     (for test-suite-results = (when should-run-tests
-							 (run-tests :suite test-suite
-								    :report-pathname nil)))
-			     (when should-run-tests
-			       (collect
-				   (list test-suite-results
-					 (testsuite-tests test-suite)
-					 (errors test-suite-results)
-					 (failures test-suite-results)))))))
-    (format t "~%")
-    (when test-results
-      (destructuring-bind (tests-run tests-error tests-failed)
-	  (reduce
-	   #'(lambda (x y)
-	       (list (+ (first x) (first y))
-		     (+ (second x) (second y))
-		     (+ (third x) (third y))))
-	   (iter (for (_ tests-run-local tests-error-local tests-failed-local) in test-results)
-		 (collect (list (length tests-run-local)
-				(length tests-error-local)
-				(length tests-failed-local)))))
-	(iter (for (test-result) in test-results)
-	      (describe-test-result test-result t))
-	(format t "~%Test Report for all tests: ~a All, ~a!"
-		tests-run
-		(if (or (> tests-failed 0) (> tests-error 0))
-		    (format nil "~a Errors, ~a Failure" tests-error tests-failed)
-		    "all passed"))))))
